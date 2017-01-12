3b5666c017e809ec9e2b177f2211587ffcf15983
Helen Hood <helen.c.hood@gmail.com>
2017-01-12 12:10:40 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+artists = [
+  {
+    name: "David Bowie",
+    albums: [],
+  },
+  {
+    name: "Prince",
+    albums: [
+      {
+        title: "Purple Rain",
+        year: "1984"
+      },
+      {
+        title: "Parade",
+        year: "1985"
+      },
+    ]
+  },
+]
+
+artists.each do |artist_attributes|
+  artist = Artist.find_or_initialize_by(
+    name: artist_attributes[:name]
+  )
+  if artist.save
+    puts "Artist saved: #{artist_attributes[:name]}"
+    artist_attributes[:albums].each do |album_attributes|
+      album = artist.albums.find_or_initialize_by(
+        title: album_attributes[:title]
+      )
+      if album.update(album_attributes)
+        puts "- Album saved: #{album_attributes[:title]}"
+      else
+        puts "- Album failed to save: #{album_attributes[:title]}"
+      end
+    end
+  else
+    puts "Artist failed to save: #{artist_attributes[:name]}"
+  end
+end
